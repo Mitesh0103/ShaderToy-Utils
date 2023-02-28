c92673ec5191e9bee8ce03742c5d49999290421f
Mitesh <mitesh2113@gmail.com>
2023-02-28 22:20:41 +0530

    Added Alpha Blending

--- Line.glsl
+++ Line.glsl
@@ -6,7 +6,7 @@ struct LineProperties
     float Thickness;
 };
 
-vec3 DrawLine(vec2 Coords, LineProperties Prop)
+vec3 DrawLine(vec2 Coords, vec3 BackBuffer, LineProperties Prop)
 {
     vec2 Delta1 = Coords - Prop.BeginAt;
     vec2 Delta2 = Prop.EndAt - Prop.BeginAt;
@@ -15,6 +15,6 @@ vec3 DrawLine(vec2 Coords, LineProperties Prop)
     float ProjectionFactor = clamp(dot(Delta1, Delta2) / dot(Delta2, Delta2), 0.0f, 1.0f);
     vec2 ProjectedVector = Delta2 * ProjectionFactor;
     float SegmentSDF = float(length(Delta1 - ProjectedVector) < Prop.Thickness);
-
-    return Prop.Color.xyz * Prop.Color.w * SegmentSDF;
+    return mix(BackBuffer, Prop.Color.xyz, Prop.Color.w * SegmentSDF);
 }
+
